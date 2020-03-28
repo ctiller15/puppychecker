e259f3e8975548d3e6795d9400bf3d51394ab0c3
Christopher <christiller2015@gmail.com>
2020-03-27 23:05:08 -0400

    update functional tests w/ expected failure.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class LandingPageTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_submit_a_photo_and_get_a_classification(self):
+        # Janet has found this new website. 
+        # You put in a picture of a dog and it tells you if it's a puppy or not!
+        # She checks out its homepage.
+        self.browser.get('http://localhost:8000')
+
+        # She notices the main page tells here where she is.
+        self.assertIn('Puppychecker', self.browser.title)
+        
+        self.fail('Finish the test!')
+
+        # She sees a prompt to input an image.
+
+        # She submits an image, and it redirects to a page with her result.
+
+        # It tells her her image is a puppy.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
