65015f967757f508986d4811eac32d1894d25ee5
Christopher <christiller2015@gmail.com>
2020-03-29 14:40:22 -0400

    Manage to save image to folder

--- pupcheck/views.py
+++ pupcheck/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+import os
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
         print("Home page, baby!")
-        print(request)
-        return HttpResponse(request.POST['file'])
+        test = request.FILES['file']
+        print(test.name)
+        print(test.size)
+        print(test.content_type)
+
+        path = os.getcwd()
+        print(path)
+
+        with open(path + '/pupcheck/saved_images/000001.jpg', 'wb+') as destination:
+            for chunk in test.chunks():
+                destination.write(chunk)
+
+        return HttpResponseRedirect('/')
     return render(request, 'home.html')
